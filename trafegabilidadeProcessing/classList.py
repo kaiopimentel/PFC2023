e4f7f0a98ed02d32035f877b4589f31159827c06
arthur-santos <arthurdears101199@gmail.com>
2023-08-01 11:01:25 -0300

    update classList

--- trafegabilidadeProcessing/classList.py
+++ trafegabilidadeProcessing/classList.py
@@ -1,31 +1,3 @@
-# class_list = [
-#             #hidrografia
-#             [
-#             "Area_Umida_A",
-#             "Banco_Areia_A",
-#             "Corredeira_A",
-#             "Foz_Maritima_L",
-#             "Massa_Dagua_A",
-#             "Recife_A",
-#             "Rocha_Em_Agua_A",
-#             "Terreno_Sujeito_Inundacao_A",
-#             "Trecho_Drenagem_L",
-#             "Trecho_Massa_Dagua_A"
-#             ]
-#             ,
-#             #vegetação
-#             [
-#             "Brejo_Pantano_A",
-#             "Caatinga_A",
-#             "Campinarana_A",
-#             "Campo_A",
-#             "Cerrado_Cerradao_A",
-#             "Floresta_A",
-#             "Mangue_A",
-#             "Veg_Cultivada_A"
-#             ]
-#         ]
-
 class_list = [
     # hidrografia
     {
