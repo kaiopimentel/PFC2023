7a74ffeb50e2ce8224d2cf6a210b901eb59d29d5
arthur-santos <arthurdears101199@gmail.com>
2023-08-02 19:33:49 -0300

    removed line vector from classList

--- trafegabilidadeProcessing/classList.py
+++ trafegabilidadeProcessing/classList.py
@@ -6,12 +6,10 @@ class_list = [
             "Area_Umida_A",
             "Banco_Areia_A",
             "Corredeira_A",
-            "Foz_Maritima_L",
             "Massa_Dagua_A",
             "Recife_A",
             "Rocha_Em_Agua_A",
             "Terreno_Sujeito_Inundacao_A",
-            "Trecho_Drenagem_L",
             "Trecho_Massa_Dagua_A"
             ]
 
